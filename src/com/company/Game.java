393a581ad87d47c9ae615f2e797455e695966cc1
Simon Morris <simonjmorris213@gmail.com>
2021-09-12 17:43:22 -0700

    collabdice

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -4,53 +4,58 @@ import java.util.ArrayList;
 
 public class Game {
 
-    private ArrayList<Player> players = new ArrayList<>();
-    private int numOfRounds;
-    private int numOfDice;
+    private ArrayList<Player> players = new ArrayList<Player>();
+    private int numRounds;
+    private int numDice;
 //    private Player winner;
-    //TODO: modify to add multiple winners
+    //TODO: Modify winner to include multiple winners
 
-    public Game(int numOfPlayers, int numOfRounds, int numOfDice) {
-        this.numOfRounds = numOfRounds;
-        this.numOfDice = numOfDice;
+    public Game(int numOfPlayers, int numRounds, int numDice) {
+        this.numRounds = numRounds;
+        this.numDice = numDice;
         generatePlayers(numOfPlayers);
+        startGame();
+    }
+
+    private void playerTurn(Player player) {
+        int sum = 0;
+
+        for (Die die: player.rollHand()) {
+            die.rollDie();
+            sum += die.getValue();
+            System.out.println(player.getName() + " rolled " + die.getValue());
+        }
+        int newScore = sum + player.getScore();
+        player.setScore(sum);
+        System.out.println(player.getName() + "'s total score is: " + sum);
     }
 
     public void startGame() {
 
-    }
-
-    public void playerTurn(Player player) {
-
-
-    }
-
-    public void printScore() {
-
-
-    }
-
-    private void generatePlayers(int numOfPlayers) {
-        for (int i = 0; i < numOfPlayers; i++) {
-            String name = CLI.getStr("What player name?\nName: ");
-            Player newPlayer = new Player(name,0);
-            newPlayer.setPlayerName(name);
-            Hand hand = new Hand();
-            hand.setDice(generateDie());
+        // Iterates through each Player object in the players Array List
+        for (Player player : players) {
+            playerTurn(player);
         }
-
     }
 
-    private ArrayList<Die> generateDie() {
+    private void generatePlayers(int numPlayers) {
+        for (int i = 0; i < numPlayers ; i++) {
+            String name = CLI.getString("Enter your name\nName: ");
+            Player newPlayer = new Player(name, 0);
+            players.add(newPlayer);
+            Hand hand = new Hand();
+            hand.setDice(generateDice());
+            newPlayer.setHand(hand);
+        }
+    }
 
-        new Hand().dice = new ArrayList<>();
+    private ArrayList<Die> generateDice() {
+        ArrayList<Die> tempArr = new ArrayList<Die>();
 
-        for (int i = 0; i < numOfDice; i++) {
+        for (int i = 0; i < numDice; i++) {
             Die newDie = new Die(6);
             tempArr.add(newDie);
         }
-        return null;
+        return tempArr;
     }
-
-
 }
